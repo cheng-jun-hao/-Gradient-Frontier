1bf2a7a2d1e253007249f6b32a59e16232852d7c
cheng-jun-hao <cjh2571849673@163.com>
2026-06-14 17:01:00 +0800

    fix: 修改 Vite base 路径为根目录

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/-Gradient-Frontier/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
